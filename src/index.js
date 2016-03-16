e753dec9a5e042f081af8b442f650cce4fdda000
Basit Ali <basitalimundia@gmail.com>
2016-03-16 11:33:07 +0500

    Context menu.

--- src/index.js
+++ src/index.js
@@ -3,5 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/Main';
 
+const remote = eRequire('electron').remote;
+const Menu = remote.Menu;
+
+var template = require("./components/application-menu");
+console.log(template);
+var menu = Menu.buildFromTemplate(template);
+Menu.setApplicationMenu(menu);
+
 // Render the main component into the dom
 ReactDOM.render(<App />, document.getElementById('app'));
